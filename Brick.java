9bd50d70537fb766332c7a838a7400b4812bc766
outlierSlug <greenteamisthebest@gmail.com>
2024-04-29 08:19:37 -0700

    Create Brick.java

---
+++ Brick.java
@@ -0,0 +1,69 @@
+import java.awt.*;
+
+public class Brick {
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+    private Color color;
+    private int health; // Number of hits required to destroy the brick
+    
+
+    public Brick(int x, int y, int width, int height, Color color, int health) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+        this.health = health;
+    }
+
+    // Getters and setters for position, width, height, color, health
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+}
