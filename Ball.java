14f92c05fd92e9252e8f7197399afdcc96dba22e
happylemon1 <65505579+happylemon1@users.noreply.github.com>
2024-05-13 20:24:17 -0700

    Added fields for velocity in x and y directions and ball radius; added a basic method to move the ball

--- Ball.java
+++ Ball.java
@@ -1,11 +1,17 @@
 public class Ball {
     private int x;
     private int y;
+    private int radius; 
+    private int dX; 
+    private int dY; 
 
     // constructor
-    public Ball(int x, int y) {
+    public Ball(int x, int y, int radius, int dX, int dY) {
         this.x = x;
         this.y = y;
+        this.radius = radius; 
+        this.dX = dX; 
+        this.dY = dY; 
     }
 
     // returns x coordinate
@@ -26,6 +32,11 @@ public class Ball {
     // sets y coordinate
     public void setY(int y) {
         this.y = y;
+    }
+
+    public void move() {
+        x += dX; 
+        y += dY;
     }
     
 }
