8fef5b47b5e94987651562f88946bc4c35e9cdf4
happylemon1 <65505579+happylemon1@users.noreply.github.com>
2024-05-15 19:56:37 -0700

    Added get and set methods to ball class + created method for ball collisions with wall

--- Ball.java
+++ Ball.java
@@ -24,6 +24,18 @@ public class Ball {
         return y;
     }
 
+    public int getRadius() {
+        return radius; 
+    }
+
+    public int getdX() {
+        return dX; 
+    }
+
+    public int getdY() {
+        return dY;
+    }
+
     // sets x coordinate
     public void setX(int x) {
         this.x = x;
@@ -32,6 +44,25 @@ public class Ball {
     // sets y coordinate
     public void setY(int y) {
         this.y = y;
+    }
+
+    public void setdX(int dX) {
+        this.dX = dX;
+    }
+
+    public void setdY(int dY) {
+        this.dY = dY;
+    }
+
+    public void WallCollision(int width, int height) {
+        if (x - radius <= 0 || x + radius >= width) {
+            dX = -dX; 
+        }
+
+        if (y - radius <= 0 || y + radius >= height) {
+            dY = -dY; 
+        }
+        
     }
 
     public void move() {
