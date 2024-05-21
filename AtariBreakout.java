4db7fd3be7cfe0fe3e714d5abf7059995b6b07c1
jeonjas25 <jasonj0618@gmail.com>
2024-05-20 23:06:49 -0700

    Update AtariBreakout.java

--- AtariBreakout.java
+++ AtariBreakout.java
@@ -2,8 +2,7 @@ public class AtariBreakout {
     // main() method
     public static void main(String[] args) {
         Paddle paddle = new Paddle(300, 640, 5, 10, 6);
-        // change dX and dY later
-        Ball ball = new Ball(350, 350, 10, 8, 4);
+        Ball ball = new Ball(350, 350, 10, 0, 4);
         BrickLayout brickLayout = new BrickLayout(8, 10, 70, 30);
         GameCanvas gameCanvas = new GameCanvas(paddle, ball, brickLayout);
 
