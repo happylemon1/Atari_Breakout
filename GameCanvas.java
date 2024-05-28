6441e07b80ae91453e78f1b320b59bcda816eb70
jeonjas25 <jasonj0618@gmail.com>
2024-05-28 13:46:58 -0700

    Display and update score, mouse movement

--- GameCanvas.java
+++ GameCanvas.java
@@ -2,8 +2,9 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.*;
 
-public class GameCanvas extends JPanel implements ActionListener {
+public class GameCanvas extends JPanel implements ActionListener, MouseMotionListener {
     private static final int SCREEN_HEIGHT = 700;
     private static final int SCREEN_WIDTH = 700;
     private BrickLayout brickLayout;
@@ -15,6 +16,7 @@ public class GameCanvas extends JPanel implements ActionListener {
     private int score = 0;
     private int lives = 3;
     private int level = 1;
+    private Font arcadeFont;
 
     public GameCanvas(Paddle paddle, Ball ball, BrickLayout brickLayout) {
         this.paddle = paddle;
@@ -26,6 +28,7 @@ public class GameCanvas extends JPanel implements ActionListener {
         this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
         this.setFocusable(true);
         this.addKeyListener(new MyKeyAdapter());
+        this.addMouseMotionListener((MouseMotionListener) this);
         JFrame gameFrame = new JFrame("Atari Breakout");
         gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         gameFrame.add(this);
@@ -33,6 +36,18 @@ public class GameCanvas extends JPanel implements ActionListener {
         gameFrame.setResizable(false);
         gameFrame.pack();
         gameFrame.setLocationRelativeTo(null);
+
+        // loading custom arcade font
+        try {
+            arcadeFont = Font.createFont(Font.TRUETYPE_FONT, new File("PressStart2P-Regular.ttf")).deriveFont(30f);
+            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            ge.registerFont(arcadeFont);
+        } 
+        catch (IOException | FontFormatException e) {
+            e.printStackTrace();
+            // Fallback to default font if custom font fails to load
+            arcadeFont = new Font("SansSerif", Font.BOLD, 30);
+        }
 
     }
 
@@ -47,9 +62,12 @@ public class GameCanvas extends JPanel implements ActionListener {
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        drawPaddle(g);
-        drawBall(g);
-        drawBricks(g);
+        if (isRunning) {
+            drawPaddle(g);
+            drawBall(g);
+            drawBricks(g);
+            displayScore(g);
+        }
     }
 
     public void drawPaddle(Graphics g) {
@@ -82,7 +100,7 @@ public class GameCanvas extends JPanel implements ActionListener {
         }
 
         if (ball.getY() + ball.getRadius() >= SCREEN_HEIGHT) {
-            lives --; 
+            lives--; 
             ball.respawn();
         }
         int newX = ball.getX() + ball.getdX(); 
@@ -120,10 +138,78 @@ public class GameCanvas extends JPanel implements ActionListener {
                     g.setColor(Color.BLACK);
                     g.drawRect(brick.getX(), brick.getY(), brick.getWidth(), brick.getHeight());
                 }
+                // if brick is destroyed and score is not counted update score
+                else if (bricks[row][col].scoreCounted() != true) {
+                    Color color = bricks[row][col].getColor();
+                    // sets brick to score counted
+                    bricks[row][col].setScoreCounted(true);
+                    // uses if and else if statements to update score according to brick's color
+                    if (color.equals(new Color(0, 255, 255))) {
+                        score += 1;
+                    }
+                    else if (color.equals(new Color(138, 43, 226))) {
+                        score += 2;
+                    }
+                    else if (color.equals(new Color(30, 144, 255))) {
+                        score += 3;
+                    }
+                    else if (color.equals(new Color(50, 205, 50))) {
+                        score += 4;
+                    }
+                    else if (color.equals(new Color(255, 255, 0))) {
+                        score += 5;
+                    }
+                    else if (color.equals(new Color(255, 165, 0))) {
+                        score += 6;
+                    }
+                    else if (color.equals(new Color(255, 69, 0))) {
+                        score += 7;
+                    }
+                    else if (color.equals(new Color(255, 105, 180))) {
+                        score += 8;
+                    }
+                
+                }
             }
         }
     }
-        
+
+    // displayScore() writes the current score  
+    public void displayScore(Graphics g) {
+        g.setColor(Color.WHITE);
+        g.setFont(arcadeFont);
+        FontMetrics metrics = g.getFontMetrics();
+        g.drawString("Score: " + score, 10, metrics.getAscent());
+    }
+
+    // mouseMoved() updates paddle movement based on mouse movement
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        // updates paddle position based on mouse position, accounting for paddle width
+        paddle.setX(e.getX() - paddle.getWidth() / 2);
+        // makes sure paddle stays in game boundaries
+        if (paddle.getX() < 0) {
+            paddle.setX(0);
+        } 
+        else if (paddle.getX() + paddle.getWidth() > SCREEN_WIDTH) {
+            paddle.setX(SCREEN_WIDTH - paddle.getWidth());
+        }
+    }
+
+    // mouseDragged() updates paddle movement if mouse is dragged
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        // updates paddle position based on mouse position, accounting for paddle width
+        paddle.setX(e.getX() - paddle.getWidth() / 2);
+        // makes sure paddle stays in game boundaries
+        if (paddle.getX() < 0) {
+            paddle.setX(0);
+        } 
+        else if (paddle.getX() + paddle.getWidth() > SCREEN_WIDTH) {
+            paddle.setX(SCREEN_WIDTH - paddle.getWidth());
+        }
+    }        
+
     @Override
     public void actionPerformed(ActionEvent e) {
         if (isRunning) {
